d8e178d817f6868e0c6b534cbd0171d6b176a3a6
wbin01 <wildenalisson@outlook.com>
2026-06-30 13:27:00 -0300

    Hello

---
+++ src/data/html_render.py
@@ -0,0 +1,233 @@
+import re
+import base64
+
+from zipfile import ZipFile
+from lxml import etree
+from docx_parser import DocxParser
+from svg_icon_to_html import SvgIconToHTML
+
+
+class HTMLRender(object):
+    def __init__(self, docx_parser: DocxParser) -> None:
+        # Args
+        self._parser = docx_parser
+
+        # Contents
+        self._start = ''
+        self._header = ''
+        self._cover = ''
+        self._title = ''
+        self._body = ''
+        self._modals = ''
+        self._footer = ''
+        self._end = ''
+        self._html = ''
+
+        self._icon_close = icon = SvgIconToHTML('close').html
+        self._icon_book = icon = SvgIconToHTML('biblius').html
+
+        self._set_html()
+
+    @property
+    def cover(self) -> str:
+        return self._cover
+
+    @property
+    def render(self) -> str:
+        return self._html
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    def save(self, path: str = '') -> None:
+        path = path if path else self._parser.path.replace('.docx', '.html')
+        with open(path, 'w') as html:
+            html.write(self._html)
+    
+    def _set_html(self) -> None:
+        # Body
+        self._body = ' <main>\n <article>\n\n'
+        for parse in self._parser.parse['body']:
+            self._body += self._set_html_body(parse)
+        self._body +='\n </article>\n </main>\n\n <footer></footer>\n'
+
+        # self._parser.print()
+        for parse in self._parser.parse['comments']:
+            self._modals += self._set_html_body(parse, True)
+
+        # Start
+        self._start = (
+            '<!DOCTYPE html>\n'
+            '<html>\n'
+            ' <head>\n'
+            f'  <title>{self._title}</title>\n'
+            '  <meta charset="utf-8">\n'
+            '  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/c'
+            'ss/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjp'
+            'PEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossor'
+            'igin="anonymous">\n'
+            '  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/'
+            'js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNk'
+            'mXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anony'
+            'mous"></script>\n'
+            '  <style>\n'
+            '   h1, h2, h3, h4, h5, h6, h7, h8, h9 { color: #3770BC }'
+            '   .post-title { color: #000000; font-size: 28pt; }\n'
+            '   .bg { background-color: #F4C559; border-radius: 3px; }\n'
+            '  </style>\n'
+            ' </head>\n'
+            ' <body>\n\n')
+
+        # End
+        self._end = f'\n </body>\n</html>'
+
+        self._html += self._start
+        self._html += self._body + '\n'
+        self._html += self._modals
+        self._html += self._end
+
+    def _set_html_body(self, parse: dict, modal: bool = False) -> str:
+            parse_tag = tag = pr = text = id_ = style = ''
+            for key, value in parse.items():
+                if key == 'tag':
+                    parse_tag = value
+                    tag = self._set_tag(value)
+
+                elif key == 'pr' and value:
+                    for pr_key, pr_value in value.items():
+                        pr += f' {pr_key}="{pr_value}"'
+
+                elif key == 'children':
+                    for run in value:
+                        text += self._set_text_run(run)
+
+                elif key == 'meta':
+                    id_ = value['id']
+
+                elif key == 'style':
+                    style = value
+
+            # End format
+            if tag == 'h1' and parse_tag == 'Title':
+                self._title = text
+
+                class_ = 'post-title'
+                if 'align' in style: class_ += f' text-' + style['align']
+                class_ = f' class="{class_}"'
+
+                tag = f'\n   <{tag}{class_}>{text}</{tag}>\n'
+                tag = f'\n  <!-- Title -->\n  <header>{tag}  </header>\n\n'
+
+            elif tag == 'div' and parse_tag == 'comment_modal':
+                tag = (
+                    f'  <!-- Modal {id_} -->\n'
+                    f'  <div class="modal fade " id="modal{id_}" tabindex="-1" '
+                    'aria-labelledby="#idLabel" aria-hidden="true" '
+                    'data-bs-theme="read">\n'
+                    '   <div class="modal-dialog modal-lg '
+                    'modal-dialog-scrollable">\n'
+                    '    <div class="modal-content">\n'
+                    '     <div class="modal-body p-0 m-0">\n\n'
+                    '      <div class="px-2 mt-2">\n'
+                    f'       {text}\n'
+                    '      </div>\n\n'
+                    '      <div class="modal-footer p-0 m-1">\n'
+                    '       <div class="d-grid gap-2 d-flex '
+                    'justify-content-end">\n'
+                    '        <button type="button" class="btn '
+                    'btn-outline-danger btn-sm border border-0" '
+                    'data-bs-dismiss="modal" aria-label="Close">\n'
+                    f'         {self._icon_close}\n'
+                    '        </button>\n'
+                    '       </div>\n      </div>\n\n     </div>\n    </div>\n'
+                    '   </div>\n  </div>\n')
+
+            elif tag == 'img':
+                class_ = styl = ''
+                if 'align' in style: class_ = f'text-' + style['align']
+
+                if 'max-width' in style:
+                    styl = f' style="max-width:{style['max-width']};"'
+
+                tag = (
+                    f'  <figure class="image {class_}">\n   '
+                    f'<{tag}{styl}{pr} />\n'
+                    '   <figcaption></figcaption>\n'
+                    '  </figure>\n')
+                
+                if not self._cover:
+                    self._cover = tag
+                    tag = f'  <!-- Cover -->\n{tag}'
+            else:
+                class_ = ''
+                if 'align' in style: class_ = f'text-' + style['align']
+                if class_: class_ = f' class="{class_}"'
+
+                tag = f'  <{tag}{class_}{pr}>{text}</{tag}>\n'
+
+            return tag
+
+    def _set_tag(self, value: str) -> str:
+        tag = value
+        if value == 'Title':
+            tag, parse_tag = 'h1', value
+        
+        elif value == 'comment_modal':
+            tag, parse_tag = 'div', value
+
+        return tag
+
+    def _set_text_run(self, run: dict) -> str:
+        text = ''
+
+        # Tag start
+        for tag in run['tags']:
+            if tag['tag'] == 'comment':
+                text += ('<a type="button" class="ref_button '
+                    'd-print-none" data-bs-toggle="modal" ')
+
+            elif tag['tag'] == 'bg':
+                text += '<span class="bg"'
+            
+            else:
+                text += f'<{tag['tag']}'
+
+            # Tag properties
+            for pr_k, pr_v in tag['pr'].items():
+                if tag['tag'] == 'comment':
+                    text += f'data-bs-target="#modal{pr_v}"'
+                else:
+                    text += f' {pr_k}="{pr_v}"'
+            text += '>'
+
+        # Text
+        if run['text'] == 'book':
+            text += self._icon_book
+        else:
+            text += run['text']
+        
+        # Tag close - Reversed
+        end_tags = []
+        for tag in run['tags']:
+            if tag['tag'] == 'comment':
+                end_tags.append(f'</a>')
+
+            elif tag['tag'] == 'bg':
+                end_tags.append(f'</span>')
+            
+            else:
+                end_tags.append(f'</{tag['tag']}>')
+
+        end_tags.reverse()
+        for end_tag in end_tags:
+            text += end_tag
+
+        return text
+
+
+if __name__ == '__main__':
+    docx = DocxParser('/home/user/Documento1.docx')
+    html = HTMLRender(docx)
+    print(html._title)
+    html.save()
---
+++ src/data/svg_icon_to_html.py
@@ -0,0 +1,59 @@
+import re
+from pathlib import Path
+from scour import scour
+
+
+PATH = Path(__file__).resolve().parent.parent.parent
+
+
+class SvgIconToHTML(object):
+    def __init__(self, name: str) -> None:
+        self._name = name
+        self._path = PATH/'media'/'icons'/'default'/f'{name}.svg' # .as_posix()
+        self._html = None
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}("{self._name}")'
+
+    @property
+    def html(self) -> str:
+        if self._html:
+            return self._html
+
+        options = scour.sanitizeOptions()
+        options.remove_metadata = True
+        options.strip_xml_prolog = True
+        # options.enable_viewboxing = True
+
+        with open(self._path, 'r', encoding='utf-8') as f:
+            svg_text = f.read()
+
+        optimized = scour.scourString(svg_text, options)
+        optimized = re.sub(r'\s*fill\s*=\s*\"#[^\"]*\"', '', optimized.strip())
+        if 'class="' not in optimized:
+            optimized = optimized.replace('<svg ', '<svg class="" ')
+
+        if 'fill="currentColor"' not in optimized:
+            optimized = optimized.replace(
+                'class="', 'fill="currentColor" class="')
+
+        if self._name == 'book':
+            optimized = optimized.replace(
+                'class="', 'style="margin:0px 0px 2px 2px;" class="')
+        else:
+            optimized = optimized.replace(
+                'class="', 'style="margin:0px 0px 2px 0px;" class="')
+
+        if self._name:
+            html = optimized.replace('<svg', f'<svg name="{self._name}"')
+        else:
+            html = optimized
+
+        self._html = html.replace('\n', '').strip()
+
+        return self._html
+
+if __name__ == '__main__':
+    icon = SvgIconToHTML('close')
+    print(icon)
+    print(icon.html)
