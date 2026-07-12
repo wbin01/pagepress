2b482d9f8c5d828702524e6c57b0dd8791591855
wbin01 <wildenalisson@outlook.com>
2026-07-12 02:13:34 -0300

    Run

--- src/data/modules/docx_html.py
+++ src/data/modules/docx_html.py
@@ -8,77 +8,6 @@ from pathlib import Path
 from zipfile import ZipFile
 
 
-class Line(object):
-    def __init__(self, xml: str) -> None:
-        self._xml = xml
-
-        self._childrens = []
-        self._properties = {}
-        self._classes = {}
-        self._styles = {}
-
-        self._set_childrens()
-        self._set_properties()
-
-    def _set_childrens(self) -> dict:
-        xml = self._xml.lstrip('<w:p>').rstrip('</w:p>').strip()
-
-        properties = re.findall(r'<w:pPr>.*</w:pPr>', xml, re.DOTALL)
-        self._xml = properties[0] if properties else ''
-
-        childrens = xml.replace(self._xml, '').split('</w:r>')
-        for child in childrens:
-            self._childrens.append(Child(child))
-
-    def _set_properties(self) -> None:
-        if '<w:jc w:val=' in self._xml:
-            align = re.findall(r'<w:jc w:val=\"([^\"]*)\"',self._xml,re.DOTALL)
-            if align: self._styles['align'] = align[0]
-
-    def __repr__(self) -> str:
-        return f'{self.__class__.__name__}(xml)'
-
-
-class Child(object):
-    def __init__(self, xml: str) -> None:
-        self._xml = xml
-
-        self._child_type = 'text'
-        self._text = ''
-        self._source = ''
-        self._classes = {}
-        self._properties = {}
-        self._tags = []
-
-        self._set_child_type()
-        self._set_content()
-        self._set_tags()
-
-    def _set_child_type(self) -> None:
-        if '<v:imagedata' in self._xml:
-            self._child_type = 'image'
-
-    def _set_content(self) -> None:
-        if self._child_type == 'text':
-            if '<w:t ' in self._xml:
-                txt = re.findall(r'<w:t [^>]+>(.*)</w:t>',self._xml, re.DOTALL)
-                if txt: self._text = txt[0]
-
-    def _set_tags(self) -> None:
-        link = re.findall(f'<w:hyperlink [^>]+>', self._xml, re.DOTALL)
-        if link:
-            id_ = re.findall(r'r:id=\"([^\"]+)\"', link[0], re.DOTALL)
-            id_ = id_[0] if id_ else ''
-
-            src = re.findall(r'w:tooltip=\"([^\"]+)\"', link[0], re.DOTALL)
-            src = src[0] if src else ''
-
-            self._tags.append({'tag': 'a', 'id': id_, 'src': src})
-
-        if '<w:b/>' in self._xml:
-            self._tags.append({'tag': 'b'})
-
-
 class DocxHTML(object):
     def __init__(self, path: str) -> None:
         self._path = Path(os.path.expanduser(path)).as_posix()
@@ -91,13 +20,20 @@ class DocxHTML(object):
 
         self._name_space_doc = {'w':
             'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
-        self._name_space_rel = {'r':
+
+        self._name_space_rels = {'r':
             'http://schemas.openxmlformats.org/package/2006/relationships'}
+        
+        self._rels = self._xml_rels.xpath(
+                '//r:Relationship', namespaces=self._name_space_rels)
+
+        self._styles = [
+            etree.tostring(xml, encoding='unicode',pretty_print=True)
+            for xml in self._xml_styles.xpath(
+                '//w:style', namespaces=self._name_space_doc)]
 
         self._parse_comments = self._get_parse_comments()
         self._parse_document = self._get_parse_document()
-        self._parse_rels = self._get_parse_rels()
-        self._parse_styles = self._get_parse_styles()
 
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}("{self._path}")'
@@ -125,7 +61,7 @@ class DocxHTML(object):
             xml = etree.tostring(xml, encoding='unicode', pretty_print=True)
             line = re.sub(r'<w:p\b[^>]*>', '<w:p>', xml, count=1)
 
-            lines.append(Line(line))
+            lines.append(Line(line, self._styles, self._rels))
 
         return lines
 
@@ -134,7 +70,7 @@ class DocxHTML(object):
 
         parse = []
         for xml in self._xml_rels.xpath(
-                '//r:Relationship', namespaces=self._name_space_rel):
+                '//r:Relationship', namespaces=self._name_space_rels):
             # # Imgage ID
             # if xml.get('Id') == parse['meta']['id']:
             #     parse['meta']['url'] = xml.get('Target')
@@ -156,29 +92,109 @@ class DocxHTML(object):
         return parse
 
 
+class Line(object):
+    def __init__(self, xml: str, styles: list, rels: list) -> None:
+        self._xml = xml
+        self._styles = styles
+        self._rels = rels
+
+        self._properties = {}
+        self._styles = {}
+        self._runs = []
+
+        self._set_runs()
+        self._set_properties()
+
+    def _set_runs(self) -> dict:
+        xml = self._xml.lstrip('<w:p>').rstrip('</w:p>').strip()
+
+        properties = re.findall(r'<w:pPr>.*</w:pPr>', xml, re.DOTALL)
+        self._xml = properties[0] if properties else ''
+
+        runs = xml.replace(self._xml, '').split('</w:r>')
+        for run in runs:
+            self._runs.append(Run(run, self._styles, self._rels))
+
+    def _set_properties(self) -> None:
+        if '<w:jc w:val=' in self._xml:
+            align = re.findall(r'<w:jc w:val=\"([^\"]*)\"',self._xml,re.DOTALL)
+            if align: self._styles['align'] = align[0]
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(xml)'
+
+
+class Run(object):
+    def __init__(self, xml: str, styles: list, rels: list) -> None:
+        self._xml = xml
+        self._styles = styles
+        self._rels = rels
+
+        self._type = 'text'
+        self._text = ''
+        self._properties = {}
+        self._tags = []
+
+        self._set_type()
+        self._set_content()
+        self._set_tags()
+
+    def _set_type(self) -> None:
+        if '<mc:AlternateContent>' in self._xml:
+            shape = re.findall(r'<v:shape .+</v:shape>', self._xml, re.DOTALL)
+            if shape:
+                if '<v:imagedata' in shape[0]:
+                    self._type = 'image'
+
+                elif '<v:path ' in shape[0] and '<v:stroke ' in shape[0]:
+                    self._type = 'draw'
+
+    def _set_content(self) -> None:
+        if self._type == 'text':
+            if '<w:t ' in self._xml:
+                txt = re.findall(r'<w:t [^>]+>(.*)</w:t>',self._xml, re.DOTALL)
+                if txt: self._text = txt[0]
+
+    def _set_tags(self) -> None:
+        link = re.findall(f'<w:hyperlink [^>]+>', self._xml, re.DOTALL)
+        if link:
+            id_ = re.findall(r'r:id=\"([^\"]+)\"', link[0], re.DOTALL)
+            id_ = id_[0] if id_ else ''
+
+            src = re.findall(r'w:tooltip=\"([^\"]+)\"', link[0], re.DOTALL)
+            src = src[0] if src else ''
+
+            self._tags.append({'tag': 'a', 'id': id_, 'src': src})
+
+        if '<w:b/>' in self._xml:
+            self._tags.append({'tag': 'b'})
+
+
 if __name__ == '__main__':
     from pprint import pprint
 
     parser = DocxHTML('~/doc.docx')
-    print(parser)
+    # print(parser)
     for line in parser._parse_document:
-        # pprint(line._xml)
+        pprint(line._xml)
         # pprint(line._properties)
 
-        for c in line._childrens:
-            print('type:')
-            pprint(c._child_type)
+        for c in line._runs:
+            # print('type:')
+            # pprint(c._type)
 
-            print('text/source:')
-            pprint(c._text) if c._child_type == 'text' else pprint(c._source)
+            # print('text:')
+            # pprint(c._text)
             
-            print('properties:')
-            pprint(c._properties)
+            # print('properties:')
+            # pprint(c._properties)
             
-            print('tags:')
-            pprint(c._tags)
+            # print('tags:')
+            # pprint(c._tags)
 
             print('xml:')
             pprint(c._xml)
 
             print('---')
+
+        print('===')
