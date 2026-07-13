2bc06779e00b0eb1c59adbfe404c48affd815727
wbin01 <wildenalisson@outlook.com>
2026-07-12 21:03:39 -0300

    Docx Parse

--- src/data/modules/docx_parse.py
+++ src/data/modules/docx_parse.py
@@ -15,32 +15,33 @@ class DocxParse(object):
         with ZipFile(self._path) as f:  # f.read(url).decode('utf-8')
             self._xml_comments = etree.parse(f.open('word/comments.xml'))
             self._xml_document = etree.parse(f.open('word/document.xml'))
-            self._xml_rels = etree.parse(f.open('word/_rels/document.xml.rels'))
-            self._xml_styles = etree.parse(f.open('word/styles.xml'))
+
+            xml_rels = etree.parse(f.open('word/_rels/document.xml.rels'))
+            xml_styles = etree.parse(f.open('word/styles.xml'))
 
         self._name_space_doc = {'w':
             'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
-
+        
         self._name_space_rels = {'r':
             'http://schemas.openxmlformats.org/package/2006/relationships'}
         
-        self._rels = self._xml_rels.xpath(
-                '//r:Relationship', namespaces=self._name_space_rels)
+        self._xml_rels = xml_rels.xpath(
+            '//r:Relationship', namespaces=self._name_space_rels)
 
-        self._styles = [
+        self._xml_styles = [
             re.sub(r'<w:style.+w:styleId=\"', '<w:style w:styleId="',
                 etree.tostring(xml, encoding='unicode', pretty_print=True),
                 count=1)
-            for xml in self._xml_styles.xpath(
+            for xml in xml_styles.xpath(
                 '//w:style', namespaces=self._name_space_doc)]
 
-        self._parse_comments = self._get_parse_comments()
-        self._parse_document = self._get_parse_document()
+        self._comments = self._get_comments()
+        self._document = self._get_document()
 
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}("{self._path}")'
 
-    def _get_parse_comments(self) -> list:
+    def _get_comments(self) -> list:
         if not self._xml_comments: return []
 
         comments = etree.tostring(
@@ -54,7 +55,7 @@ class DocxParse(object):
 
         return parse
 
-    def _get_parse_document(self) -> list:
+    def _get_document(self) -> list:
         if not self._xml_document: return []
 
         lines = []
@@ -62,7 +63,6 @@ class DocxParse(object):
                 '//w:body/w:p', namespaces=self._name_space_doc):
             xml = etree.tostring(xml, encoding='unicode', pretty_print=True)
             line = re.sub(r'<w:p\b[^>]*>', '<w:p>', xml, count=1)
-
             lines.append(Line(line, self))
 
         return lines
@@ -73,20 +73,19 @@ class Line(object):
         self._xml = xml
         self._parent = parent
 
-        self._xml_styles = self._parent._styles
-        self._xml_rels = self._parent._rels
+        self._xml_styles = self._parent._xml_styles
+        self._xml_rels = self._parent._xml_rels
         self._path = self._parent._path
 
-        self._type = 'Paragraph'
+        self._type = self._set_type()
+        self._runs = self._set_runs()
+        self._styles = self._set_styles()
         self._properties = {}
-        self._styles = {}
-        self._runs = []
 
-        self._set_type()
-        self._set_runs()
-        self._set_properties()
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(xml)'
 
-    def _set_type(self) -> None:
+    def _set_type(self) -> str:
         if '<w:pStyle w:val="' in self._xml:
             id_ = re.findall(
                 r'<w:pStyle w:val=\"(\d+)\"/>', self._xml, re.DOTALL)
@@ -95,39 +94,32 @@ class Line(object):
             for style in self._xml_styles:
                 if re.findall(fr'<w:style w:styleId=\"{id_[0]}\">', style):
                     type_ = re.findall(r'<w:name w:val=\"([^\"]+)\"/>', style)
-                    if type_: self._type = type_[0]
+                    if type_: return type_[0]
+        return 'Paragraph'
 
-    def _set_runs(self) -> None:
+    def _set_runs(self) -> list:
         xml = self._xml.lstrip('<w:p>').rstrip('</w:p>').strip()
 
         properties = re.findall(r'<w:pPr>.*</w:pPr>', xml, re.DOTALL)
         self._xml = properties[0] if properties else ''
 
-        runs = xml.replace(self._xml, '').split('</w:r>')
-        for run in runs:
+        runs = []
+        for run in xml.replace(self._xml, '').split('</w:r>'):
             run = Run(run, self._parent)
-            if self._run_is_valid(run): self._runs.append(run)
+            if self._run_is_valid(run): runs.append(run)
+        return runs
 
-    def _set_properties(self) -> None:
+    def _set_styles(self) -> dict:
+        styles = {}
         if '<w:jc w:val=' in self._xml:
             align = re.findall(r'<w:jc w:val=\"([^\"]*)\"',self._xml,re.DOTALL)
-            if align: self._styles['align'] = align[0]
+            if align: styles['align'] = align[0]
+        return styles
 
     def _run_is_valid(self, run) -> bool:
-        conditions = [
-            run._type == 'Text',
-            not run._text,
-            not run._properties,
-            not run._tags,
-            not run._meta]
-
-        if all(conditions):
+        if run._type == 'Text' and not run._text:
             return False
         return True
-
-
-    def __repr__(self) -> str:
-        return f'{self.__class__.__name__}(xml)'
 
 
 class Run(object):
@@ -135,43 +127,44 @@ class Run(object):
         self._xml = xml
         self._parent = parent
 
-        self._xml_styles = self._parent._styles
-        self._xml_rels = self._parent._rels
+        self._xml_styles = self._parent._xml_styles
+        self._xml_rels = self._parent._xml_rels
         self._path = self._parent._path
 
         self._img_as_base64 = False
         self._xml_shape = ''
-
-        self._type = 'Text'
-        self._text = ''
-        self._properties = {}
-        self._tags = []
         self._meta = {}
 
-        self._set_type()
-        self._set_text()
-        self._set_properties()
-        self._set_tags()
+        self._type = self._set_type()
+        self._text = self._set_text()
+        self._properties = self._set_properties()
+        self._tags = self._set_tags()
 
-    def _set_type(self) -> None:
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(xml)'
+        
+    def _set_type(self) -> str:
         if '<mc:AlternateContent>' in self._xml:
             shape = re.findall(r'<v:shape .+</v:shape>', self._xml, re.DOTALL)
             if shape:
                 self._xml_shape = shape[0]
 
                 if '<v:imagedata' in shape[0]:
-                    self._type = 'Image'
+                    return 'Image'
 
                 elif '<v:path ' in shape[0] and '<v:stroke ' in shape[0]:
-                    self._type = 'Draw'
+                    return 'Draw'
+        return 'Text'
 
-    def _set_text(self) -> None:
+    def _set_text(self) -> str:
         if self._type == 'Text':
             if '<w:t ' in self._xml:
                 txt = re.findall(r'<w:t [^>]+>(.*)</w:t>',self._xml, re.DOTALL)
-                if txt: self._text = txt[0]
+                if txt: return txt[0]
+        return ''
 
-    def _set_properties(self) -> None:
+    def _set_properties(self) -> dict:
+        properties = {}
         if self._type == 'Image':
             id_ = src = ext = width = height = ''
 
@@ -196,15 +189,18 @@ class Run(object):
                 h = re.findall(r'height:(\d+)', self._xml_shape)
                 if h: height = int(int(h[0]) * (96 / 72))
 
-            self._properties['src'] = src
-            self._properties['width'] = width
-            self._properties['height'] = height
+            properties['src'] = src
+            properties['width'] = width
+            properties['height'] = height
             self._meta['extension'] = ext
 
         elif self._type == 'Draw':
             pass
 
+        return properties
+
     def _set_tags(self) -> None:
+        tags = []
         link = re.findall(f'<w:hyperlink [^>]+>', self._xml, re.DOTALL)
         if link:
             id_ = re.findall(r'r:id=\"([^\"]+)\"', link[0], re.DOTALL)
@@ -213,7 +209,7 @@ class Run(object):
             href = re.findall(r'w:tooltip=\"([^\"]+)\"', link[0], re.DOTALL)
             href = href[0] if href else ''
 
-            self._tags.append({'tag': 'a', 'href': href})
+            tags.append({'tag': 'a', 'href': href})
 
         if self._type == 'Image':
             href = target = ''
@@ -228,7 +224,7 @@ class Run(object):
                             target = rel.get('TargetMode')
             if href:
                 if target == 'External': target = '_blank'
-                self._tags.append({'tag': 'a', 'href': href, 'target': target})
+                tags.append({'tag': 'a', 'href': href, 'target': target})
 
 
         if '<w:highlight w:val="' in self._xml:
@@ -236,33 +232,38 @@ class Run(object):
                 f'<w:highlight w:val=\"([^\"]*)\"', self._xml, re.DOTALL)
 
             if highlight[0] != 'none':
-                self._tags.append({
+                tags.append({
                     'tag': 'span',
                     'class': f'highlight-{highlight[0]}',
                     'style': f'background-color: {highlight[0]};'})
 
         if '<w:b/>' in self._xml:
-            self._tags.append({'tag': 'b'})
+            tags.append({'tag': 'b'})
 
         if '<w:strike/>' in self._xml:
-            self._tags.append({'tag': 's'})
+            tags.append({'tag': 's'})
 
         if '<w:u w:val="single"/>' in self._xml:
-            self._tags.append({'tag': 'u'})
+            tags.append({'tag': 'u'})
 
         if '<w:i/>' in self._xml:
-            self._tags.append({'tag': 'i'})
+            tags.append({'tag': 'i'})
+
+        return tags
+
 
 if __name__ == '__main__':
     from pprint import pprint
 
     parser = DocxParse('~/doc.docx')
     # print(parser)
-    for line in parser._parse_document:
-        # print('type: ', end='')
-        # pprint(line._type)
-        # print('properties: ', end='')
-        # pprint(line._properties)
+    for line in parser._document:
+        print('type: ', end='')
+        pprint(line._type)
+        print('properties: ', end='')
+        pprint(line._properties)
+        print('styles: ', end='')
+        pprint(line._styles)
         # print('xml: ', end='')
         # pprint(line._xml)
         for c in line._runs:
